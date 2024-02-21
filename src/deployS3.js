2613ed9da927f751884705825cffdcb3e3fe072a
ByambadorjB <btbyambadorj@gmail.com>
2024-02-21 19:20:44 +1100

    deployS3.js is to deploy S3 bucket for data storage

---
+++ src/deployS3.js
@@ -0,0 +1,42 @@
+// deployS3.js
+
+const AWS = require('aws-sdk');
+
+async function deployS3Bucket(){
+    
+    const s3 = new AWS.S3({
+        accessKeyId: 'AKIAXZO6SAUMZBVQHHJL',
+        secretAccessKey: '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',
+        region: 'ap-southeast-2'
+    });
+
+    const bucketName = 'xmops-data-bucket';
+
+    // Check if the bucket already exists
+    s3.headBucket({Bucket: bucketName}, (err, data) => {
+        if(err){
+            if(err.statusCode === 404) {
+                // Bucket does not exist, create it
+                s3.createBucket({Bucket: bucketName}, (error, data) => {
+                    if(error){
+                        console.error('Failed to create bucket: ', error);
+                    } else {
+                        console.log('Bucket created successfully: ', data.Location);
+                    }
+                });
+            } else {
+                console.error('Error checking bucket: ', err);
+            }
+        } else{
+            console.log('Bucket already exists: ', bucketName);
+        }
+    });
+   
+};
+
+//deployS3Bucket();
+
+
+// Export the function so it can be used in other modules
+module.exports = deployS3Bucket;
+
